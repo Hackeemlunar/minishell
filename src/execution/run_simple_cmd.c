e95af54b6afd5d88b2fa9b0461688f846dace6a5
Hackeem Mensah Bosu <hmensah-@lab2r2s12.42abudhabi.ae>
2025-05-16 15:45:52 +0400

    changed the return of run_simple_cmd

--- src/execution/run_simple_cmd.c
+++ src/execution/run_simple_cmd.c
@@ -6,7 +6,7 @@
 /*   By: hmensah- <hmensah-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 14:38:27 by hmensah-          #+#    #+#             */
-/*   Updated: 2025/05/15 19:34:03 by hmensah-         ###   ########.fr       */
+/*   Updated: 2025/05/15 20:07:36 by hmensah-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,6 @@ int	run_simple_cmd(t_ast *ast, t_mshell *shell, t_allocs *allocs,
 	if (pid == 0)
 		run_child_cmd(ast, shell, allocs, table);
 	waitpid(pid, &status, 0);
-	printf("status is %d", status);
 	if (WIFEXITED(status))
 		set_exit_status(shell, WEXITSTATUS(status));
 	else if (WIFSIGNALED(status))
